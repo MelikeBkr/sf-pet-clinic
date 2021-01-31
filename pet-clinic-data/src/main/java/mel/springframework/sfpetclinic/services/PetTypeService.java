8ea72e6d7c5f0dd34eb0a2ef167f60395dcd9b33
Melike Bakır <39765068+MelikeBkr@users.noreply.github.com>
2021-01-31 20:23:52 +0000

    PetType Map Service was implemented. Closes #28

---
+++ pet-clinic-data/src/main/java/mel/springframework/sfpetclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package mel.springframework.sfpetclinic.services;
+
+import mel.springframework.sfpetclinic.model.PetType;
+
+public interface PetTypeService extends  CrudService<PetType,Long>
+{
+
+}
