1e6c74000ca4c4d4f6f4987d8468acd010508d8e
Melike Bakır <39765068+MelikeBkr@users.noreply.github.com>
2021-02-02 22:44:45 +0000

    Revert "PetTypes with Bootstrap were added. Issie 30 not resolved yet.  This reverts commit 62c0f486377e819bcdb8d870869ddc718bd84675

--- pet-clinic-web/src/main/java/mel/springframework/sfpetclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/mel/springframework/sfpetclinic/bootstrap/DataInitializer.java
@@ -1,10 +1,8 @@
 package mel.springframework.sfpetclinic.bootstrap;
 
 import mel.springframework.sfpetclinic.model.Owner;
-import mel.springframework.sfpetclinic.model.PetType;
 import mel.springframework.sfpetclinic.model.Vet;
 import mel.springframework.sfpetclinic.services.OwnerService;
-import mel.springframework.sfpetclinic.services.PetTypeService;
 import mel.springframework.sfpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,25 +12,14 @@ public class DataInitializer implements CommandLineRunner
 {
     private final OwnerService ownerService;
     private final VetService vetService;
-    private final PetTypeService petTypeService;
 
-    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataInitializer(OwnerService ownerService, VetService vetService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
-        this.petTypeService = petTypeService;
     }
 
     @Override
-    public void run(String... args) throws Exception
-    {
-        PetType dog = new PetType();
-        dog.setName("dog");
-        PetType savedDogPetType = petTypeService.save(dog);
-
-        PetType cat = new PetType();
-        dog.setName("cat");
-        PetType savedCatPetType = petTypeService.save(cat);
-
+    public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
         owner1.setId(1L);
         owner1.setFirstName("Elizabeth");
