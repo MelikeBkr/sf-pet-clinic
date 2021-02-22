58edbe9e002d525de8fe9029610d9d5efa3e90a8
Melike Bakır <39765068+MelikeBkr@users.noreply.github.com>
2021-02-22 22:41:43 +0000

    Vet was converted to JPA Entity. Closes #33, Closes #35

--- pet-clinic-data/src/main/java/mel/springframework/sfpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/mel/springframework/sfpetclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package mel.springframework.sfpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name="vets")
 public class Vet extends Person {
+    //FetchType in default is LAZY
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name="vet_specialities", joinColumns = @JoinColumn(name="vet_id"),
+            inverseJoinColumns = @JoinColumn(name="speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
