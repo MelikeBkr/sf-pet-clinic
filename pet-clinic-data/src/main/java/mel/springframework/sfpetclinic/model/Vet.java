955571be205d1bef4fc58c076eb346a949994019
Melike Bakır <39765068+MelikeBkr@users.noreply.github.com>
2021-02-25 16:59:41 +0000

    Project Lombok was added. Closes #50

--- pet-clinic-data/src/main/java/mel/springframework/sfpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/mel/springframework/sfpetclinic/model/Vet.java
@@ -1,11 +1,18 @@
 package mel.springframework.sfpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "vets")
 public class Vet extends Person {
 
@@ -13,12 +20,4 @@ public class Vet extends Person {
     @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
             inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
-
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }
