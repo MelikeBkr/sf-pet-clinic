f95bc3564b423ce42442249a162a2e0a5a509269
Melike Bakır <39765068+MelikeBkr@users.noreply.github.com>
2021-02-28 20:03:27 +0000

    Upgraded to JUnit 5

--- pet-clinic-web/src/test/java/mel/springframework/sfpetclinic/SfPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/mel/springframework/sfpetclinic/SfPetClinicApplicationTests.java
@@ -1,11 +1,13 @@
 package mel.springframework.sfpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SfPetClinicApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
