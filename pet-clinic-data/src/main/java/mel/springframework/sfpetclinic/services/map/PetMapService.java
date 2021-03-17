87bebfa3a33c5155fb96474e35b25a0778bea2ca
Melike Bakır <39765068+MelikeBkr@users.noreply.github.com>
2021-03-17 16:49:05 +0000

    Create or update pet functionality is not functioning properly. The relevant html files and unit tests are added/created.

--- pet-clinic-data/src/main/java/mel/springframework/sfpetclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/mel/springframework/sfpetclinic/services/map/PetMapService.java
@@ -2,10 +2,12 @@ package mel.springframework.sfpetclinic.services.map;
 
 import mel.springframework.sfpetclinic.model.Pet;
 import mel.springframework.sfpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 @Service
+@Profile({"default", "map"})
 public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
